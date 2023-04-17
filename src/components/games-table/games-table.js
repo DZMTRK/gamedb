99a843ad280c3e695c5e8ff41ecd4fdcb5cbe03e
Dzmitry Mishkovich <5286082@gmail.com>
2023-04-17 15:47:48 +0300

    feat: change table field names

--- src/components/games-table/games-table.js
+++ src/components/games-table/games-table.js
@@ -2,24 +2,13 @@ import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 
 const columns = [
-    { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'firstName', headerName: 'First name', width: 130 },
-    { field: 'lastName', headerName: 'Last name', width: 130 },
-    {
-      field: 'age',
-      headerName: 'Age',
-      type: 'number',
-      width: 90,
-    },
-    {
-      field: 'fullName',
-      headerName: 'Full name',
-      description: 'This column has a value getter and is not sortable.',
-      sortable: false,
-      width: 160,
-      valueGetter: (params) =>
-        `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-    },
+    { field: 'id', headerName: 'ID', type: 'int', width: 70 },
+    { field: 'title', headerName: 'Title', type: 'str', width: 130 },
+    { field: 'year', headerName: 'Year', type: 'int', width: 130 },
+    { field: 'genre', headerName: 'Genre', type: 'enum', width: 90 },
+    { field: 'raiting', headerName: 'Raiting', type: 'float', width: 90 },
+    { field: 'developer', headerName: 'Developer', type: 'str', width: 90 },
+    { field: 'publisher', headerName: 'Publisher', type: 'str', width: 90 },
   ];
   
   const rows = [
