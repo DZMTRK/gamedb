3db3adf241c90a9c9ecd54a7deb08975ec2350c9
Dzmitry Mishkovich <5286082@gmail.com>
2023-04-20 12:41:52 +0300

    feature: show game list in table

--- src/components/games-table/games-table.js
+++ src/components/games-table/games-table.js
@@ -1,14 +1,17 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 import { getGameData } from './get-game-data'
 
-getGameData()
-  .then((data) => {
-      console.log(data)
-});
 
+function GamesTable() {
+  
+  const [rows, setRows] = useState([]);
 
-const columns = [
+  getGameData().then((data) => {
+    setRows(data);
+  });
+
+  const columns = [
     { field: 'id', headerName: 'ID', type: 'int', width: 70 },
     { field: 'title', headerName: 'Title', type: 'str', width: 130 },
     { field: 'year', headerName: 'Year', type: 'int', width: 130 },
@@ -17,17 +20,9 @@ const columns = [
     { field: 'developer', headerName: 'Developer', type: 'str', width: 90 },
     { field: 'publisher', headerName: 'Publisher', type: 'str', width: 90 },
   ];
-
-  const rows = 
-  [
-    { id: 1, title: 'demotest', year: '2023', genre: 'demotest' },
-    { id: 2, title: 'demotest', year: '2023', genre: 'demotest' },
-    { id: 3, title: 'demotest', year: '2023', genre: 'demotest' },
-  ];
-
-function GamesTable() {
+  
   return (
-    <div style={{ height: 700, width: '100%' }}>
+    <div style={{ height: 650, width: '100%' }}>
       <DataGrid
         rows={rows}
         columns={columns}
