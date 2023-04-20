d7a9d08ab720b58e01a53921dac3a9324e9a60a6
Dzmitry Mishkovich <5286082@gmail.com>
2023-04-20 15:33:00 +0300

    feature: change column width

--- src/components/games-table/games-table.js
+++ src/components/games-table/games-table.js
@@ -4,11 +4,11 @@ import { getGameData } from '../get-game-data'
 
 const columns = [
   { field: 'id', headerName: 'ID', type: 'int', width: 70 },
-  { field: 'title', headerName: 'Title', type: 'str', width: 400 },
+  { field: 'title', headerName: 'Title', type: 'str', flex: 1 },
   { field: 'year', headerName: 'Year', type: 'int', width: 70 },
   { field: 'genre', headerName: 'Genre', type: 'enum', flex: 1 },
   { field: 'raiting', headerName: 'Raiting', type: 'float', width: 70 },
-  { field: 'developer', headerName: 'Developer', type: 'str', flex: 1 },
+  { field: 'developer', headerName: 'Developer', type: 'str', width: 200 },
   { field: 'publisher', headerName: 'Publisher', type: 'str', flex: 1 },
 ];
 
@@ -21,7 +21,7 @@ function GamesTable() {
   });
   
   return (
-    <div className="tableDataGrid">
+    <div>
       <DataGrid
         rows={rows}
         columns={columns}
