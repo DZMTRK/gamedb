2f68d28d721d387c283ff1499c6ac0b29334f907
Dzmitry Mishkovich <5286082@gmail.com>
2023-04-20 14:37:44 +0300

    feature: change row height to auto

--- src/components/games-table/games-table.js
+++ src/components/games-table/games-table.js
@@ -2,6 +2,15 @@ import React, { useState } from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 import { getGameData } from './get-game-data'
 
+const columns = [
+  { field: 'id', headerName: 'ID', type: 'int', width: 70 },
+  { field: 'title', headerName: 'Title', type: 'str', width: 130 },
+  { field: 'year', headerName: 'Year', type: 'int', width: 130 },
+  { field: 'genre', headerName: 'Genre', type: 'enum', width: 90 },
+  { field: 'raiting', headerName: 'Raiting', type: 'float', width: 90 },
+  { field: 'developer', headerName: 'Developer', type: 'str', width: 90 },
+  { field: 'publisher', headerName: 'Publisher', type: 'str', width: 90 },
+];
 
 function GamesTable() {
   
@@ -10,25 +19,15 @@ function GamesTable() {
   getGameData().then((data) => {
     setRows(data);
   });
-
-  const columns = [
-    { field: 'id', headerName: 'ID', type: 'int', width: 70 },
-    { field: 'title', headerName: 'Title', type: 'str', width: 130 },
-    { field: 'year', headerName: 'Year', type: 'int', width: 130 },
-    { field: 'genre', headerName: 'Genre', type: 'enum', width: 90 },
-    { field: 'raiting', headerName: 'Raiting', type: 'float', width: 90 },
-    { field: 'developer', headerName: 'Developer', type: 'str', width: 90 },
-    { field: 'publisher', headerName: 'Publisher', type: 'str', width: 90 },
-  ];
   
   return (
-    <div style={{ height: 650, width: '100%' }}>
+    <div className="tableDataGrid">
       <DataGrid
         rows={rows}
         columns={columns}
+        autoHeight = {true}
         pageSize={5}
         rowsPerPageOptions={[5]}
-        //checkboxSelection
       />
     </div>
   );
