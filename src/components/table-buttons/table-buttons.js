8d91edcac0dd5e3aa08456c1d960aa68b817808c
Dzmitry Mishkovich <5286082@gmail.com>
2023-04-26 16:58:18 +0300

    feature: remove delete button

--- src/components/table-buttons/table-buttons.js
+++ src/components/table-buttons/table-buttons.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
-import DeleteIcon from '@mui/icons-material/Delete';
 
 const TableButtons = () => {
     return (
-        <Stack direction="row" spacing={2}>
-            <Button variant="contained">+ Add Item</Button>
-            <Button variant="contained" endIcon={<DeleteIcon />} color="warning">Delete Item</Button>
-        </Stack>
+        <Button variant="contained">+ Add Item</Button>
     )
 }
 
