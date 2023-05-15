d634d724677c31b65809f1f19d9e5bd3c1175169
Dzmitry Mishkovich <5286082@gmail.com>
2023-05-15 19:52:12 +0300

    feature: add edit option for the table items

--- src/components/games-table/games-table.js
+++ src/components/games-table/games-table.js
@@ -9,12 +9,12 @@ const url = 'http://localhost:3002/game/';
 
 const columns = [
   // { field: 'id', headerName: 'ID', type: 'int', width: 70 },
-  { field: 'title', headerName: 'Title', type: 'str', flex: 3 },
-  { field: 'year', headerName: 'Year', type: 'int', flex: 1 },
-  { field: 'genre', headerName: 'Genre', type: 'enum', flex: 3 },
-  { field: 'raiting', headerName: 'Raiting', type: 'float', flex: 1 },
-  // { field: 'developer', headerName: 'Developer', type: 'str', width: 200 },
-  // { field: 'publisher', headerName: 'Publisher', type: 'str', flex: 1 },
+  { field: 'title', headerName: 'Title', type: 'str', flex: 3, editable: true },
+  { field: 'year', headerName: 'Year', type: 'int', flex: 1, editable: true },
+  { field: 'genre', headerName: 'Genre', type: 'enum', flex: 3, editable: true },
+  { field: 'raiting', headerName: 'Raiting', type: 'float', flex: 1, editable: true },
+  { field: 'developer', headerName: 'Developer', type: 'str', width: 200, editable: true },
+  { field: 'publisher', headerName: 'Publisher', type: 'str', flex: 1, editable: true },
 ];
 
 function GamesTable() {
