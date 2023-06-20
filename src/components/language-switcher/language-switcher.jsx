21c9f4a671cd6f1f53e94b321a34ab48846df46f
Dzmitry Mishkovich <5286082@gmail.com>
2023-06-20 16:20:38 +0300

    fix: select language button now works proprly

--- src/components/language-switcher/language-switcher.jsx
+++ src/components/language-switcher/language-switcher.jsx
@@ -2,21 +2,25 @@ import React, { useCallback } from 'react'
 
 import { useTranslation } from 'react-i18next'
 
+const lngs = {
+  en: { nativeName: 'English' },
+  ru: { nativeName: 'Russian' },
+}
+
 function LanguageSwitcher() {
   const { i18n } = useTranslation()
-
-  const lngs = {
-    en: { nativeName: 'English' },
-    ru: { nativeName: 'Russian' },
-  }
-
-  const handleLanguageSwitch = useCallback(lng => i18n.changeLanguage(lng), [i18n])
+  const handleLanguageSwitch = useCallback(event => i18n.changeLanguage(event.target.name), [i18n])
 
   return (
     <div>
       {Object.keys(lngs).map(lng => (
-        // eslint-disable-next-line react/jsx-no-bind
-        <button key={lng} style={{ fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal' }} type="submit" onClick={handleLanguageSwitch}>
+        <button
+          key={lng}
+          name={lng}
+          style={{ fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal' }}
+          type="submit"
+          onClick={handleLanguageSwitch}
+        >
           {lngs[lng].nativeName}
         </button>
       ))}
