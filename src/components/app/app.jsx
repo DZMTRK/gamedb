fbcd1af700949d5c61df5dd22b53311502743078
Dzmitry Mishkovich <5286082@gmail.com>
2023-06-20 11:42:42 +0300

    refactor:change language swithcer to the separate component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,6 +3,7 @@ import React, { Suspense } from 'react'
 import { useTranslation } from 'react-i18next'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
+import LanguageSwitcher from '../language-switcher'
 import Page404 from '../pages/page404'
 
 const GamesTable = React.lazy(() => import('../games-table'))
@@ -10,15 +11,8 @@ const GamesTable = React.lazy(() => import('../games-table'))
 function App() {
   const rootPath = '/'
   const path404 = '/404'
-  const lngs = {
-    en: { nativeName: 'English' },
-    ru: { nativeName: 'Russian' },
-  }
-  const { t, i18n } = useTranslation()
-  // const langSelectorStyle = lng => {
-  //   const fontWeight = i18n.resolvedLanguage === lng ? 'bold' : 'normal'
-  //   return fontWeight
-  // }
+
+  const { t } = useTranslation()
   return (
     <Router>
       <div>
@@ -31,14 +25,7 @@ function App() {
                 <header>
                   <div>
                     <h1>{t('description.h1')}</h1>
-                    <div>
-                      {Object.keys(lngs).map(lng => (
-                        // eslint-disable-next-line react/jsx-no-bind
-                        <button key={lng} style={{ fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal' }} type="submit" onClick={() => i18n.changeLanguage(lng)}>
-                          {lngs[lng].nativeName}
-                        </button>
-                      ))}
-                    </div>
+                    <LanguageSwitcher />
                   </div>
                 </header>
                 <section>
