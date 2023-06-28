0849399f22d9016ac5da34b8ee221755d6ace456
Dzmitry Mishkovich <5286082@gmail.com>
2023-06-28 21:29:25 +0300

    fix: mutateElement works fine in cases when BE is unavalible

--- src/service/mutateElement.js
+++ src/service/mutateElement.js
@@ -1,5 +1,7 @@
 import { editElement } from '../api/API'
 
-const mutateElement = (newItem, oldItem) => editElement(newItem).then(() => newItem).catch(() => oldItem)
+const mutateElement = (newItem, oldItem) => editElement(newItem)
+  .then(() => newItem)
+  .catch(() => oldItem)
 
 export default mutateElement
