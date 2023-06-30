37feebd4820e24217e1ec8bdc00100d17053762c
Dzmitry Mishkovich <5286082@gmail.com>
2023-06-30 15:31:01 +0300

    feature: add some style to main page

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,9 +3,12 @@ import React, { Suspense } from 'react'
 import { useTranslation } from 'react-i18next'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
+import logo from '../../img/logo.png'
 import LanguageSwitcher from '../language-switcher'
 import Page404 from '../pages/page404'
 import * as pagelist from '../pages/pagelist'
+
+import './app.css'
 
 const GamesTable = React.lazy(() => import('../games-table'))
 
@@ -21,9 +24,14 @@ function App() {
             element={
               <div>
                 <header>
-                  <div>
+                  <div id="logoAndHeader">
+                    <div id="logoContainer">
+                      <img id="logo" src={logo} alt="" />
+                    </div>
                     <h1>{t('description.h1')}</h1>
-                    <LanguageSwitcher />
+                    <div id="navigation">
+                      <LanguageSwitcher />
+                    </div>
                   </div>
                 </header>
                 <section>
